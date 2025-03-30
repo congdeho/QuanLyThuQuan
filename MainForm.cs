db85bbe608ddec91e9ed2360a1914f98b0f588cf
nvhao100604 <164126733+nvhao100604@users.noreply.github.com>
2025-03-30 21:22:41 +0700

    haodeptrai push ti giaodien

--- MainForm.cs
+++ MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -43,6 +44,81 @@ namespace QuanLyThuQuan
         }
 
         private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deviceType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                AddDeviceForm addDeviceForm = new AddDeviceForm();
+                addDeviceForm.ShowDialog();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            UpdateDeviceForm updateDeviceForm = new UpdateDeviceForm();
+            updateDeviceForm.ShowDialog();
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label28_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label34_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label36_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label29_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
         {
 
         }
--- obj/Release/net8.0-windows/QuanLyThuQuan.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/QuanLyThuQuan.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyThuQuan")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d158354355a8a4609e63544ffd4746a3f2d165b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyThuQuan")]
